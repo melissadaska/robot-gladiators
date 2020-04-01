7eda151e15ad6f7ad740ea05bb6c0b303811e694
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-04-01 17:43:31 -0500

    fix player prompt bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -62,8 +62,21 @@ var fight = function(enemy) {
 
 }
 
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
+};
+
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
